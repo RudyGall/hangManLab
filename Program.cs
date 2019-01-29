dca56e247a9cc745a7ae077eb852d69f07d677e2
Rudy Gall <rudyegall@gmail.com>
2019-01-29 16:47:01 -0500

    Formatted main to be readable

--- Program.cs
+++ Program.cs
@@ -10,18 +10,23 @@ namespace HangManStarterKit
     {
         static void Main(string[] args)
         {
-            Player p1 = new ABCplayer();
-            Player p2 = new HumanPlayer();
-            Player p3 = new BruteForcePlayer();
-            Player p4 = new SmartyPlayer();
-            Player p5 = new RandomPlayer();
+            //////******UNCOMMENT OUT THE PLAYER YOU WOULD LIKE TO PLAY******//////
+            
 
-            HangmanGame hg1 = new HangmanGame(p1);
-            HangmanGame hg2 = new HangmanGame(p2);
-            HangmanGame hg3 = new HangmanGame(p3);
-            HangmanGame hg4 = new HangmanGame(p4);
-            HangmanGame hg5 = new HangmanGame(p5);
+            //Player p1 = new ABCplayer();
+            //HangmanGame hg1 = new HangmanGame(p1);
 
+            //Player p2 = new HumanPlayer();
+            //HangmanGame hg2 = new HangmanGame(p2);
+
+            //Player p3 = new BruteForcePlayer();
+            //HangmanGame hg3 = new HangmanGame(p3);
+
+            //Player p4 = new SmartyPlayer();
+            //HangmanGame hg4 = new HangmanGame(p4);
+
+            //Player p5 = new RandomPlayer();
+            //HangmanGame hg5 = new HangmanGame(p5);
         }
     }
 }
